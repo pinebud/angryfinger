fb52a0cb032e2220323c5b6dcb4f29d63106e368
jing.ning <jing.ning@zuora.com>
2013-08-18 21:30:48 +0800

    Test commit

---
+++ patterns/algrithm/datastructure/apps/Probe2Sum.java
@@ -0,0 +1,10 @@
+package datastructure.apps;
+
+public class Probe2Sum {
+	
+	public static int probe2Sum(Integer[] inputArray, int lowbound, int upperbound){
+		int numOfT =0;
+		return numOfT;
+	}
+
+}
