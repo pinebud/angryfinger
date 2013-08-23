6240d9cd1cbbd3b176c28b5b9b033c9f72993759
jing.ning <jing.ning@zuora.com>
2013-08-23 22:01:03 +0800

    Calculate shortest path

--- patterns/algrithm/graph/Graph.java
+++ patterns/algrithm/graph/Graph.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Directed Graph
+ * This data structure can be used for both undirected Graph and directed Graph
+ * But it has redundant duplicated information which may consume too much memory for large volume
  * @author jingning
  *
  */
 public class Graph {
-	private Map<String,Vertex> vertices = new HashMap<String,Vertex>();
+	private Map<String,Vertex> vertexMap = new HashMap<String,Vertex>();
+	
+	private Map<String, Edge> edgeMap = new HashMap<String, Edge>();
 	
 	private List<Edge> edges = new ArrayList<Edge>();
 	
@@ -20,18 +23,44 @@ public class Graph {
 			String[] tempStrs = rawNode.split("\t");
 			String vertexId = tempStrs[0];
 			Vertex v = new Vertex(vertexId);
-			vertices.put(vertexId, v);
+			vertexMap.put(vertexId, v);
 			int len = tempStrs.length;
 			for(int i=1;i<len;i++){
 				String[] adjVertex_weight = tempStrs[i].trim().split(",");
-				
+				String adjVertexId = adjVertex_weight[0];
+				String weightString = adjVertex_weight[1];
+				int weight = Integer.valueOf(weightString);
+				Vertex adjVertex = addVertex(adjVertexId, v);				
+				Edge edge = new Edge(v,adjVertex);
+				edge.setWeight(weight);
+				edgeMap.put(vertexId+"-"+adjVertexId, edge);
+				edges.add(edge);
 			}
 		}
 	}
 	
-	public Path calculateShortPath(String startVertexID, String endVertexID){
-		Path p = new Path();
-		return p;
+	private Vertex addVertex(String vertexId, Vertex adjVertex){
+		Vertex v = vertexMap.get(vertexId);
+		if(v==null){
+			v = new Vertex(vertexId);
+			v.AddAdjVertex(adjVertex);
+			vertexMap.put(vertexId, v);
+		}else{
+			v.AddAdjVertex(adjVertex);
+		}
+		return v;
+	}
+	
+	public Vertex getVertexById(String vertexId){
+		return vertexMap.get(vertexId);
+	}
+	
+	public Edge getEdge(Vertex source, Vertex target){
+		return edgeMap.get(source.getId()+"-"+target.getId());
+	}
+	
+	public List<Edge> getEdgeList(){
+		return edges;
 	}
 	
 }
--- patterns/algrithm/graph/shortestpath/ShortestPath.java
+++ patterns/algrithm/graph/shortestpath/ShortestPath.java
@@ -1,10 +1,21 @@
 package graph.shortestpath;
 
-import graph.Graph;
+import graph.*;
 
 public class ShortestPath {
+	
+	private Graph g = null;
 
-	public ShortestPath(Graph g){
+	public ShortestPath(Graph graph){
+		g = graph;
+	}
+
+	public Path calculateShortestPath(String startVertexID, String endVertexID){
+		Path p = new Path();
+		Vertex startV = g.getVertexById(startVertexID);
+		Vertex endV = g.getVertexById(endVertexID);
 		
+		
+		return p;
 	}
 }
