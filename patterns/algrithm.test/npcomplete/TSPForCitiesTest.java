2bb01d301f7187c17e8c15d281f175e247d087c4
jing.ning <jing.ning@zuora.com>
2013-10-13 22:50:37 +0800

    TSP is done with Greedy+Randomize

--- patterns/algrithm.test/npcomplete/TSPForCitiesTest.java
+++ patterns/algrithm.test/npcomplete/TSPForCitiesTest.java
@@ -2,6 +2,7 @@ package npcomplete;
 
 import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 import junit.framework.Assert;
 
@@ -11,21 +12,56 @@ import others.Constants;
 import util.FileUtility;
 
 public class TSPForCitiesTest {
+	private static final File inputFile_quiz = new File(Constants.RESOURCE_FOLDER+"/npcomplete/tsp.txt");
 	private static final File inputFile_test00 = new File(Constants.RESOURCE_FOLDER+"/npcomplete/tsp_test00.txt");
-	
-	@Test
+	private static final File inputFile_test01 = new File(Constants.RESOURCE_FOLDER+"/npcomplete/tsp_test01.txt");
+	private static final File inputFile_test02 = new File(Constants.RESOURCE_FOLDER+"/npcomplete/tsp_test02.txt");
+//	@Test
 	public void testSqrt(){
 		int rlt = (int)( Math.sqrt(6.25)+1.2);
 		Assert.assertEquals(3, rlt);
 	}
 
 //	@Test
-	public void testSimple(){
-		List<String> lineList = FileUtility.readFileByLineAsStringList(inputFile_test00);
+	public void testRandResult(){
+		for(int i=0;i<10;i++){
+			Random rand = new Random();
+			int randInt = rand.nextInt(3);
+			System.out.println(randInt-1);
+		}
+	}	
+
+	@Test
+	public void quiz(){
+		List<String> lineList = FileUtility.readFileByLineAsStringList(inputFile_quiz);
 		TSPForCities instance = new TSPForCities(lineList);
-		int rlt = instance.calculate();
-		int expected =(int)( Math.sqrt(10.25)+Math.sqrt(9.5)+7.5);
-		Assert.assertEquals(expected, rlt);
+		double rlt = instance.calculate(10000);
+		Assert.assertEquals(26442, (int)rlt);
 	}
 
+	
+	@Test
+	public void testSimple0(){
+		List<String> lineList = FileUtility.readFileByLineAsStringList(inputFile_test00);
+		TSPForCities instance = new TSPForCities(lineList);
+		double rlt = instance.calculate(100);
+		int expected =(int)( Math.sqrt(10.25)+Math.sqrt(9.5)+7.5+6);
+		Assert.assertEquals(expected, (int)rlt);
+	}
+
+	@Test
+	public void testSimple1(){
+		List<String> lineList = FileUtility.readFileByLineAsStringList(inputFile_test01);
+		TSPForCities instance = new TSPForCities(lineList);
+		double rlt = instance.calculate(100);
+		Assert.assertEquals(37, (int)rlt);
+	}
+	
+	@Test
+	public void testSimple2(){
+		List<String> lineList = FileUtility.readFileByLineAsStringList(inputFile_test02);
+		TSPForCities instance = new TSPForCities(lineList);
+		double rlt = instance.calculate(100);
+		Assert.assertEquals(73, (int)rlt);
+	}
 }
--- patterns/algrithm/npcomplete/TSPForCities.java
+++ patterns/algrithm/npcomplete/TSPForCities.java
@@ -1,16 +1,20 @@
 package npcomplete;
 
+import java.nio.charset.CodingErrorAction;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+
+import org.junit.Test;
 
 public class TSPForCities {
 	private int N = 0;
 	private Cordinate[] cordinates = null;
 	private double[][] distances = null;
-//	private Map<Integer,Cordinate> map = new HashMap<Integer, Cordinate>();
-//	private Map<Cordinate,Integer> map = new HashMap<Cordinate,Integer>();
+	
 	
 	public TSPForCities(List<String> lineList){
 		N = Integer.valueOf(lineList.get(0).trim());
@@ -24,9 +28,6 @@ public class TSPForCities {
 			Cordinate cordi = new Cordinate(x, y);
 			cordi.setId(i-1);
 			cordinates[i-1] = cordi;
-//			map.put(i-1,cordi);
-//			map.put(cordi, i-1);
-			
 		}
 		for(int i=0;i<N;i++){	
 			Cordinate a = cordinates[i];
@@ -43,42 +44,85 @@ public class TSPForCities {
 		}
 	}
 	
-	public int calculate(){
-		int rlt = -1;
-		Cordinate[] array = Arrays.copyOf(cordinates, 2*N);
-		Arrays.sort(array);
-		tsp(array, 0 , N, 0);
-		//TODO: Calculate result
+	public double calculate(){
+		Cordinate[] array = Arrays.copyOf(cordinates, N);
+		mysort(array);
+		List<Cordinate> list = new LinkedList<Cordinate>();
+		list.add(array[0]);
+		double tpsLength = 0;
+		for(int i=1;i<N;i++){
+			Cordinate w = array[i];
+			int size = list.size();
+			double minDelta=Double.MAX_VALUE;
+			int insertAfter = 0;
+			for(int j=0;j<size;j++){
+				Cordinate u = list.get(j);
+				Cordinate v = list.get((j+1)%size);
+				double dist_u_v = distances[u.getId()][v.getId()];
+				double dist_u_w = distances[u.getId()][w.getId()];
+				double dist_w_v = distances[w.getId()][v.getId()];
+				double delta =dist_u_w + dist_w_v - dist_u_v;
+				if(delta<minDelta){
+					minDelta = delta;
+					insertAfter = j+1;
+				}
+			}
+			list.add(insertAfter, w);
+			tpsLength+=minDelta;
+		}
+		if(tpsLength<18){
+			System.out.println(list.toString());
+			double sum = distances[list.get(list.size()-1).getId()][list.get(0).getId()];
+			for(int i=0;i<list.size()-1;i++){
+				double dist = distances[list.get(i).getId()][list.get(i+1).getId()];
+				sum+=dist;
+			}
+			System.out.println(sum);
+		}
+		return tpsLength;
+	}
+	
+	public double calculate(int tryingCount){
+		double min = Double.MAX_VALUE;
+		for(int i=0;i<tryingCount;i++){
+			double rlt = calculate();
+//			System.out.println(rlt);
+			if(rlt<min)
+				min = rlt;
+		}
+		return min;
+	}
+	
+	private double calculate1(){
+		Cordinate[] array = Arrays.copyOf(cordinates, N);
+//		Arrays.sort(array);
+		mysort(array);
+//		print(array,0,N);
+		Cordinate[] sortArray = Arrays.copyOf(array, 2*N);
+		for(int i=N,j=0;j<N;i++,j++){
+			sortArray[i]=array[j];
+		}
+		double rlt = tsp(sortArray, 0 , N-1, N);
+//		print(sortArray, 0, N );
 		return rlt;
 	}
 	
-	private void tsp(Cordinate[] array, int low, int high, int offset){
-		int n = high - low+1;
-		int mid = low+n/2;
+	private double tsp(Cordinate[] array, int low, int high, int offset){
 		int newoffset = (offset+N)%(2*N);
+		if(low==high){
+			// Base case
+			array[newoffset+low] = array[offset+low];
+			return 0;
+		}
+		int n = high - low;
+		int mid = low+n/2;
 		int current_low = offset+low, current_high = offset+high;
-		if(n<3){
-			//TODO: Base case
-			for(int i=0;i<n;i++){
-				array[newoffset+low+i] = array[current_low+i];
-			}
-		}
 		int current_mid = offset+ mid;
-		tsp(array, low, mid, newoffset);
-		tsp(array, mid+1, high, newoffset);
-		//TODO: Merge two sub-cycle.
+		double len_of_left_cycle = tsp(array, low, mid, newoffset);
+		double len_of_right_cycle = tsp(array, mid+1, high, newoffset);
+		//Merge two sub-cycle.
 		double min_distance = Double.MAX_VALUE;
-		double len_of_two_cycle = 0.0; 
-		for(int i=current_low;i<current_mid;i++){
-			len_of_two_cycle+=distances[array[i].getId()][array[i+1].getId()];
-		}
-		len_of_two_cycle=+distances[array[current_mid].getId()][array[current_low].getId()];
-		
-		for(int i=current_mid+1;i<current_high;i++){
-			len_of_two_cycle+=distances[array[i].getId()][array[i+1].getId()];
-		}
-		len_of_two_cycle=+distances[array[current_high].getId()][array[current_mid+1].getId()];
-		
+		double len_of_two_cycle = len_of_left_cycle+len_of_right_cycle; 
 		for(int i=current_low;i<=current_mid;i++){
 			int left1 = array[i].getId();
 			int left2 = -1;
@@ -97,21 +141,35 @@ public class TSPForCities {
 				double removed_distance_sum = distances[left1][left2]+distances[right1][right2];
 				double delta_distance1 = distances[left1][right1]+distances[left2][right2]-removed_distance_sum;
 				double delta_distance2 = distances[left1][right2]+distances[left2][right1]-removed_distance_sum;
-				double new_len = len_of_two_cycle-removed_distance_sum+(delta_distance1<delta_distance2?delta_distance1:delta_distance2);
+				double new_len = len_of_two_cycle+(delta_distance1<delta_distance2?delta_distance1:delta_distance2);
 				if(new_len<min_distance){
 					min_distance = new_len;
+					int k = newoffset+low;
+					for(int m=current_low;m<=i;k++,m++){
+						array[k]=array[m];
+					}
 					if(delta_distance1<delta_distance2){
-						for(int k=low;k<=i-current_low;k++){
-							array[newoffset+k]=array[offset+k];
+						for(int m = j; m>current_mid; m--,k++){
+							array[k] = array[m];							
 						}
-//						array[newoffset+k]+array[]
+						for(int m=current_high;m>j;m--,k++){
+							array[k] = array[m];
+						}
 					}else{
-						
-					}					
+						for(int m = j+1; m<=current_high; m++,k++){
+							array[k] = array[m];							
+						}
+						for(int m=current_mid+1;m<=j;m++,k++){
+							array[k] = array[m];
+						}
+					}
+					for(int m=i+1;m<=current_mid;k++,m++){
+						array[k]=array[m];
+					}
 				}
 			}
 		}
-		
+		return min_distance;
 	}
 	
 	class Cordinate implements Comparable<Cordinate>{
@@ -133,22 +191,41 @@ public class TSPForCities {
 			return id;
 		}
 		
-		public double getDistance(Cordinate a, Cordinate b){
-			return Math.sqrt(Math.pow(a.x-b.x, 2)+Math.pow(a.y-b.y, 2));
-		}
-
 		@Override
 		public int compareTo(Cordinate o) {
-			if(radius<o.radius)
-				return -1;
-			else if(radius > o.radius)
-				return 1;
-			return 0;
+			Random rand = new Random();
+			int randInt = rand.nextInt(3);
+			return randInt-1;
+//			if(radius<o.radius)
+//				return -1;
+//			else if(radius > o.radius)
+//			else if(x>o.x)
+//			return 0;
 		}
 		
 		public String toString(){
 			return String.valueOf(id);
 		}
 	}
-		
+	
+	private void print(Object[] array, int from, int to){
+		StringBuilder str = new StringBuilder();
+		for(int i=from;i<to;i++){
+			str.append(array[i]+",");			
+		}
+		System.out.println(str.substring(0, str.length()-1));
+	}
+	
+	private void mysort(Object[] array){
+		Random rand = new Random();
+		int cnt = rand.nextInt(N);
+		for(int i =0;i<cnt;i++){
+		int s = rand.nextInt(N);
+		int t = rand.nextInt(N);
+		Object temp = array[t];
+		array[t] = array[s];
+		array[s] = temp;
+		}
+	}
+	
 }
